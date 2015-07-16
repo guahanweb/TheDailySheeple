784125f1396a0800f9bdce9b2dd7e43abe0374ed
Garth Henson <grth.a.henson@disney.com>
2015-07-16 16:21:47 -0700

    Updating markup

--- sidebar-left-single.php
+++ sidebar-left-single.php
@@ -4,7 +4,7 @@
     $posts = get_posts('numberposts=10&orderby=date&tag=featured');
     foreach ($posts as $post): ?>
 
-        <article class="mini" id="post-<?php echo $post->ID; ?>" class="post-<?php echo $post->ID; ?> post type-post status-publish" >
+        <article id="post-<?php echo $post->ID; ?>" class="post-<?php echo $post->ID; ?> post type-post status-publish">
             <h1 class="entry-title"><?php printf('<a href="%s" rel="bookmark">%s</a>', get_permalink($post->ID), $post->post_title); ?></h1>
         </article>
 
