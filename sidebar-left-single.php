4541fd2a574ea8f447728f15c8de5e767be98d1d
Garth Henson <grth.a.henson@disney.com>
2015-07-16 16:28:12 -0700

    Setting up ads

--- sidebar-left-single.php
+++ sidebar-left-single.php
@@ -1,8 +1,14 @@
 <header><h1>Featured</h1></header>
 <div class="articles articles-side articles-featured">
     <?php
-    $posts = get_posts('numberposts=10&orderby=date&tag=featured');
-    foreach ($posts as $post): ?>
+    $posts = get_posts('numberposts=25&orderby=date&tag=featured');
+    foreach ($posts as $k => $post): ?>
+
+    <?php if ($k === 10): ?>
+        <article class="ads ads-side">
+            <p>Here go the ads!</p>
+        </article>
+    <?php endif; ?>
 
         <article id="post-<?php echo $post->ID; ?>" class="post-<?php echo $post->ID; ?> post post-mini type-post status-publish">
             <h1 class="entry-title"><?php printf('<a href="%s" rel="bookmark">%s</a>', get_permalink($post->ID), $post->post_title); ?></h1>
