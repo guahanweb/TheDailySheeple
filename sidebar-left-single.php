b818bb4f5637e19bd614f4afd2ccd7589a52a12b
Garth Henson <grth.a.henson@disney.com>
2015-07-16 16:17:29 -0700

    Link the title of featured images

--- sidebar-left-single.php
+++ sidebar-left-single.php
@@ -1,14 +1,14 @@
 <header><h1>Featured</h1></header>
-<section class="articles articles-featured">
+<div class="articles articles-featured">
     <?php
     $posts = get_posts('numberposts=10&orderby=date&tag=featured');
     foreach ($posts as $post): ?>
 
-        <article class="mini">
-            <header>
-                <h1><?php printf('<a href="%s">%s</a>', get_permalink($post->ID), $post->post_title); ?></h1>
+        <article class="mini" id="post-<?php echo $post->ID; ?>" class="post-<?php echo $post->ID; ?> post type-post status-publish">
+            <header class="entry-title">
+                <h1><?php printf('<a href="%s" rel="bookmark">%s</a>', get_permalink($post->ID), $post->post_title); ?></h1>
             </header>
         </article>
 
     <?php endforeach; ?>
-</section>
+</div>
