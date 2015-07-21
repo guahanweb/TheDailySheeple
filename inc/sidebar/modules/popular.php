1ad1dd84e97541683966ee6afd8cf03e83e2b6dd
Garth Henson <grth.a.henson@disney.com>
2015-07-21 13:31:09 -0700

    Adding method check

--- inc/sidebar/modules/popular.php
+++ inc/sidebar/modules/popular.php
@@ -4,12 +4,16 @@
     </header>
     <div class="articles">
 <?php
-$res = stats_get_csv('postviews', array(
-    'days' => -1,
-    'limit' => -1
-));
+if (!function_exists('stats_get_csv')) {
+    echo '<p><code>stats_get_csv</code> does not exist</p>';
+} else {
+    $res = stats_get_csv('postviews', array(
+        'days' => -1,
+        'limit' => -1
+    ));
 
-echo "<pre>" . print_r($res, TRUE) . "</pre>\n";
+    echo "<pre>" . print_r($res, TRUE) . "</pre>\n";
+}
 ?>
     </div>
 </div>
