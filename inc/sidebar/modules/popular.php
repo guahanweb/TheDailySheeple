00f9341b7926601a390c6c57e74854e185291686
Garth Henson <grth.a.henson@disney.com>
2015-07-21 13:35:17 -0700

    Updating stats call to retrieve only 10 posts

--- inc/sidebar/modules/popular.php
+++ inc/sidebar/modules/popular.php
@@ -4,16 +4,19 @@
     </header>
     <div class="articles">
 <?php
-if (!function_exists('stats_get_csv')) {
-    echo '<p><code>stats_get_csv</code> does not exist</p>';
-} else {
-    $res = stats_get_csv('postviews', array(
-        'days' => -1,
-        'limit' => -1
-    ));
+$res = stats_get_csv('postviews', array(
+    'days' => -1,
+    'limit' => 50
+));
 
-    echo "<pre>" . print_r($res, TRUE) . "</pre>\n";
+$posts = array();
+foreach ($res as $post) {
+    if (count($posts) < 10 && $post['post_id'] && get_post($post['post_id']) && 'post' === get_post_type($post['post_id'])) {
+        $posts[] = $post;
+    }
 }
+
+echo '<pre>' . print_r($posts, true) . '</pre>';
 ?>
     </div>
 </div>
