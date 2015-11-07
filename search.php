c63d0c67472738bd57baca3d23a3aef174852bc3
Garth Henson <garth.a.henson@disney.com>
2015-11-07 13:59:11 -0800

    Setting up search page to match post template

--- search.php
+++ search.php
@@ -8,40 +8,43 @@
  */
 
 get_header(); ?>
-        <section class="column column-triple" id="main-column">
-            <div class="container">
-                <section class="column column-one-third left-column">
-					<header><h1>Left Column</h1></header>
-				</section>
-				<section class="column column-two-third">
-					<header>
-						<h1 class="page-title"><?php printf(__('Search Results for: %s', 'thedailysheeple'), get_search_query()); ?></h1>
-					</header>
-					<?php if ( have_posts() ) : ?>
-					<div class="articles">
-				<?php
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
+    <section id="main-column" class="content-area column column-triple">
+        <div class="container">
+            <section class="column column-one-third left-column">
+                <?php get_sidebar('left-single'); ?>
+            </section>
+            <section class="column column-two-third site-content" id="main" role="main">
+                <header>
+                    <h1 class="page-title"><?php printf(__('Search Results for: %s', 'thedailysheeple'), get_search_query()); ?></h1>
+                </header>
+                <?php if ( have_posts() ) : ?>
+                <div class="articles">
+                <?php
+                    // Start the Loop.
+                    while ( have_posts() ) : the_post();
 
-						/*
-						 * Include the post format-specific template for the content. If you want to
-						 * use this in a child theme, then include a file called called content-___.php
-						 * (where ___ is the post format) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
+                        /*
+                         * Include the post format-specific template for the content. If you want to
+                         * use this in a child theme, then include a file called called content-___.php
+                         * (where ___ is the post format) and that will be used instead.
+                         */
+                        get_template_part( 'content', get_post_format() );
 
-					endwhile;
-					// Previous/next post navigation.
-					thedailysheeple_paging_nav();
-					echo "</div>\n";
-				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
+                    endwhile;
+                    // Previous/next post navigation.
+                    thedailysheeple_paging_nav();
+                    echo "</div>\n";
+                else :
+                    // If no content, include the "No posts found" template.
+                    get_template_part( 'content', 'none' );
 
-				endif;
-			?>
-				</section>
-			</div>
-		</section>
-<?php get_sidebar('ads');
+                endif;
+                ?>
+                </div>
+            </section>
+        </div>
+    </section>
+<?php
+get_sidebar( 'content' );
+get_sidebar( 'ads' );
 get_footer();
