4b3aa8cf1a2d3e3bcd99087fffaeb6c5dad937e1
Garth Henson <garth.a.henson@disney.com>
2015-11-07 20:13:47 -0800

    Fixing sidebar date

--- main-archive.php
+++ main-archive.php
@@ -7,7 +7,7 @@ query_posts(array(
 ));
 ?>
 <header>
-    <a href="<?php echo date('/Y/m/', strtotime('1 month ago')); ?>"><?php echo __('See Full Archives', 'thedailysheeple'); ?></a>
+    <a href="<?php echo date('/Y/m/', mktime(date('Y'), date('m'))); ?>"><?php echo __('See Full Archives', 'thedailysheeple'); ?></a>
     <h1><?php echo __('Sheeple News Archives', 'thedailysheeple'); ?></h1>
 </header>
 <div class="articles">
