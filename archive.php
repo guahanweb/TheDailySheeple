40f14bf99057f3e225458ee0c667d705983b2325
Garth Henson <garth.a.henson@disney.com>
2015-11-07 19:35:26 -0800

    Fixing archives

--- archive.php
+++ archive.php
@@ -4,33 +4,33 @@
  */
 
 get_header(); ?>
-	<section class="column column-triple" id="main-column">
-		<div class="container">
-			<section class="column column-one-third left-column">
-				<header><h1>Left Column</h1></header>
-			</section>
-			<section class="column column-two-third">
-				<header>
-				<?php
-				$obj = get_queried_object();
-				printf('<h1>%s</h1>', $obj->name);
-				?>
-				</header>
-				<div class="articles">
-				<?php
-				if (have_posts()):
-					while(have_posts()) : the_post();
-					get_template_part('content', get_post_format());
-					endwhile;
-					thedailysheeple_paging_nav();
-				else:
-					get_template_part('content', 'none');
-				endif;
-				?>
-				</div>
-			</section>
-		</div>
-	</section>
-	<?php 
-get_sidebar('ads');
+    <section id="main-column" class="content-area column column-triple">
+        <div class="container">
+            <section class="column column-one-third left-column">
+                <?php get_sidebar('left-single'); ?>
+            </section>
+            <section class="column column-two-third site-content" id="main" role="main">
+                <header>
+                <?php
+                $obj = get_queried_object();
+                printf('<h1>%s</h1>', $obj->name);
+                ?>
+                </header>
+                <div class="articles">
+                <?php
+                if (have_posts()):
+                    while(have_posts()) : the_post();
+                    get_template_part('content', get_post_format());
+                    endwhile;
+                    thedailysheeple_paging_nav();
+                else:
+                    get_template_part('content', 'none');
+                endif;
+                ?>
+                </div>
+            </section>
+        </div>
+    </section>
+    <?php
+get_sidebar('content');
 get_footer();
