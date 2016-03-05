214e26fed66c5916ff07dd4a152a1c190960e597
Garth Henson <garth.a.henson@disney.com>
2016-03-05 14:04:25 -0800

    Prep for new subscription changes

--- sidebar.php
+++ sidebar.php
@@ -1,81 +1,96 @@
-
 <!-- START GARTH ORIGINAL CODE -->
 
-        <section class="column column-single" id="right-column">
-            <div class="container">
-                <section class="ad-list">
-             <div class="ad-block">   
-<!-- START DIRECT AD JMB -->
-                      <div class="ad ad-JMB">
-                          <a href="http://www.jmbullion.com/silver/" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'JMB-banner-bison']);"><img border="0" src="/images/banners/JMB-fast-free-shipping-250x250.jpg" width=300 height=300 /></a>
-                        </div>
-<!-- END DIRECT AD JMB -->
-<!-- FMT Ad Through MAR 18, 2016 -->
-   <div class="ad ad-FMT">
-                          <a href="http://FutureMoneyTrends.com/GoldStock" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'FMT-BRI-Banner1']);"><img border="0" src="http://www.thedailysheeple.com/wp-content/uploads/2016/03/BRI-300.jpg" /></a>
-                        </div>
-<!-- End FMT Ad -->
-                    
-<div class="ad ad-TTG"><a href="http://toptiergearusa.com/?utm_source=thedailysheeple.com&utm_medium=banner&utm_content=Sidebar-Right&utm_campaign=TTG-Sidebar-Right-Banner" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'TTG-RightBannerTop']);" ><img src="http://www.thedailysheeple.com/wp-content/uploads/2016/03/biomask2-300.png" alt="" width="300" height="300" border="0" /></a></div>
-           <?php
+<section class="column column-single" id="right-column">
+  <div class="container">
+    <section class="ad-list">
+        <div class="ad-block">
+          <?php
             the_widget('TDS_MailChimp_Widget', array(), array(
                 'before_widget' => '',
                 'after_widget' => '',
                 'before_title' => '',
                 'after_title' => ''
             ));
-            ?>
+          ?>
 
-<!-- SHUFFLING AD UNITS -->
-<div id="region-sidebarrightshuffle">
+          <!-- START DIRECT AD JMB -->
+          <div class="ad ad-JMB">
+            <a href="http://www.jmbullion.com/silver/" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'JMB-banner-bison']);"><img border="0" src="/images/banners/JMB-fast-free-shipping-250x250.jpg" width=300 height=300 /></a>
+          </div>
+          <!-- END DIRECT AD JMB -->
+          <!-- FMT Ad Through MAR 18, 2016 -->
+          <div class="ad ad-FMT">
+            <a href="http://FutureMoneyTrends.com/GoldStock" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'FMT-BRI-Banner1']);"><img border="0" src="http://www.thedailysheeple.com/wp-content/uploads/2016/03/BRI-300.jpg" /></a>
+          </div>
+          <!-- End FMT Ad -->
 
-<!-- START Direct Ads JHL CAMPING SUPPLY -->
-<div class="sidebar_right_ad"><a href="http://www.campingsurvival.com/fishantibiotics.html" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'CampingSurvival-banner2']);" ><img src="/images/banners/campingsurvival-fishmox2.jpg" alt="" BORDER=0 WIDTH=300 HEIGHT=300 /></a></div>
+          <div class="ad ad-TTG">
+            <a href="http://toptiergearusa.com/?utm_source=thedailysheeple.com&utm_medium=banner&utm_content=Sidebar-Right&utm_campaign=TTG-Sidebar-Right-Banner" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'TTG-RightBannerTop']);"><img src="http://www.thedailysheeple.com/wp-content/uploads/2016/03/biomask2-300.png" alt="" width="300" height="300" border="0" /></a>
+          </div>
 
-<div class="sidebar_right_ad">
-<A HREF="http://www.campingsurvival.com/lifestraw.html" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'CampingSurvival-banner']);"><IMG SRC="http://www.thedailysheeple.com/wp-content/uploads/2015/12/lifestraw2.jpg" BORDER=0 WIDTH=300 HEIGHT=300 ></A>
-</div>
-<!-- END  Direct Ads JHL CAMPING SUPPLY -->
+          <!-- SHUFFLING AD UNITS -->
+          <div id="region-sidebarrightshuffle">
 
-<!-- START Direct Ad SOLO STOVE-->
-<div class="sidebar_right_ad"><a href="http://www.solostove.com/?utm_source=TheDailySheeple.com&utm_medium=banner&utm_campaign=TheDailySheeple.com" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'SoloStove']);" ><img src="/images/banners/250x250-solostove.jpg" alt="" width="300" height="300" border="0" /></a></div>
-<!-- END Direct Ad SOLOSTOVE -->
-
-<!-- START Direct Ad DOTCOMMER -->
-<div class="sidebar_right_ad"><iframe src="http://dcmr.sitescoutadserver.com/disp?pid=8C31C68DDF&rnd=[CACHE-BUSTING-ID-HERE]" width='300' height='300' marginwidth='0' marginheight='0' scrollbars='0' scrolling='no' frameborder='0' bordercolor='#000000' vspace='0' hspace='0'></iframe></div>
-<!-- END Direct Ad DOTTCOMMER -->
-
-<!-- START Direct Ads Directive21 -->
-<div class="sidebar_right_ad">
-<A HREF="http://www.directive21.com/" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'Directive21-Banner']);"><IMG SRC="/images/banners/250x208-LPC-berkey.jpg" BORDER=0 WIDTH=300 HEIGHT=300 ></A>
-</div>
-
-<div class="sidebar_right_ad">
-<A HREF="http://www.directive21.com/product-category/survival-cave-food-storage/" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'Directive21-Banner-Cavefood']);"><IMG SRC="/images/banners/250x250-cavefood-directive21.jpg" BORDER=0 WIDTH=300 HEIGHT=300 ></A>
-</div>
-<!-- END Direct Ads Directive21 -->
-
-
-
-</div><!-- END region-sidebarrightshuffle DIV -->
-
-<!-- END SHUFFLING AD UNITS -->
-<!-- AMAZON CPM -->
-  <div class="ad ad-AmazonMain">
-<script type="text/javascript" language="javascript" src="//c.amazon-adsystem.com/aax2/getads.js"></script>
-<script type="text/javascript" language="javascript">
-//<![CDATA[
-aax_getad_mpb({
-  "slot_uuid":"5eddd92e-a15b-4e83-8567-2d34bd7f3e0f"
-});
-//]]>
-</script>
-</div>
-<!-- END AMAZON CPM -->
-
-                    </div>
-                </section>
+            <!-- START Direct Ads JHL CAMPING SUPPLY -->
+            <div class="sidebar_right_ad">
+              <a href="http://www.campingsurvival.com/fishantibiotics.html" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'CampingSurvival-banner2']);"><img src="/images/banners/campingsurvival-fishmox2.jpg" alt="" BORDER=0 WIDTH=300 HEIGHT=300 /></a>
             </div>
-        </section>
+
+            <div class="sidebar_right_ad">
+              <A HREF="http://www.campingsurvival.com/lifestraw.html" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'CampingSurvival-banner']);">
+                <IMG SRC="http://www.thedailysheeple.com/wp-content/uploads/2015/12/lifestraw2.jpg" BORDER=0 WIDTH=300 HEIGHT=300>
+              </A>
+            </div>
+            <!-- END  Direct Ads JHL CAMPING SUPPLY -->
+
+            <!-- START Direct Ad SOLO STOVE-->
+            <div class="sidebar_right_ad">
+              <a href="http://www.solostove.com/?utm_source=TheDailySheeple.com&utm_medium=banner&utm_campaign=TheDailySheeple.com" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'SoloStove']);"><img src="/images/banners/250x250-solostove.jpg" alt="" width="300" height="300" border="0" /></a>
+            </div>
+            <!-- END Direct Ad SOLOSTOVE -->
+
+            <!-- START Direct Ad DOTCOMMER -->
+            <div class="sidebar_right_ad">
+              <iframe src="http://dcmr.sitescoutadserver.com/disp?pid=8C31C68DDF&rnd=[CACHE-BUSTING-ID-HERE]" width='300' height='300' marginwidth='0' marginheight='0' scrollbars='0' scrolling='no' frameborder='0' bordercolor='#000000' vspace='0' hspace='0'></iframe>
+            </div>
+            <!-- END Direct Ad DOTTCOMMER -->
+
+            <!-- START Direct Ads Directive21 -->
+            <div class="sidebar_right_ad">
+              <A HREF="http://www.directive21.com/" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'Directive21-Banner']);">
+                <IMG SRC="/images/banners/250x208-LPC-berkey.jpg" BORDER=0 WIDTH=300 HEIGHT=300>
+              </A>
+            </div>
+
+            <div class="sidebar_right_ad">
+              <A HREF="http://www.directive21.com/product-category/survival-cave-food-storage/" target="_blank" rel="nofollow" onClick="_gaq.push(['_trackEvent', 'Banner', 'Click', 'Directive21-Banner-Cavefood']);">
+                <IMG SRC="/images/banners/250x250-cavefood-directive21.jpg" BORDER=0 WIDTH=300 HEIGHT=300>
+              </A>
+            </div>
+            <!-- END Direct Ads Directive21 -->
+
+
+
+          </div>
+          <!-- END region-sidebarrightshuffle DIV -->
+
+          <!-- END SHUFFLING AD UNITS -->
+          <!-- AMAZON CPM -->
+          <div class="ad ad-AmazonMain">
+            <script type="text/javascript" language="javascript" src="//c.amazon-adsystem.com/aax2/getads.js"></script>
+            <script type="text/javascript" language="javascript">
+              //<![CDATA[
+              aax_getad_mpb({
+                "slot_uuid": "5eddd92e-a15b-4e83-8567-2d34bd7f3e0f"
+              });
+              //]]>
+            </script>
+          </div>
+          <!-- END AMAZON CPM -->
+
+        </div>
+    </section>
+  </div>
+</section>
 
 <!-- END GARTH ORIGINAL CODE -->
