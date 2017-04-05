20ca53346ede2f1e0781e88d6f4f703e2d73e787
Garth Henson <garth@guahanweb.com>
2017-04-05 15:25:44 -0700

    content.php change

--- content-050816.php
+++ content-050816.php
@@ -12,6 +12,7 @@
 if (is_single()):
     edit_post_link( __('Edit', 'thedailysheeple'), '<span class="edit-link">[', ']</span>');
     the_title('<header><h1 class="entry-title single-title">', '</h1></header>');
+    do_action('tds_article_after_title');
     if ('post' == get_post_type()) { ?>
     <h2 class="posted"><?php thedailysheeple_posted_on(true); ?></h2>
     <?php } ?>
