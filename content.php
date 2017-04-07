62fc3019bc74d79cb17adce3a809cc23c75f23a0
Garth Henson <garth@guahanweb.com>
2017-04-06 21:59:31 -0700

    Updating styles

--- content.php
+++ content.php
@@ -11,10 +11,8 @@
 
 if (is_single()):
     edit_post_link( __('Edit', 'thedailysheeple'), '<span class="edit-link">[', ']</span>');
-    echo '<header>';
-    the_title('<h1 class="entry-title single-title">', '</h1>');
+    the_title('<header><h1 class="entry-title single-title">', '</h1></header>');
     do_action('tds_article_after_title');
-    echo '</header>';
     if ('post' == get_post_type()) { ?>
     <h2 class="posted"><?php thedailysheeple_posted_on(true); ?></h2>
     <?php } ?>
